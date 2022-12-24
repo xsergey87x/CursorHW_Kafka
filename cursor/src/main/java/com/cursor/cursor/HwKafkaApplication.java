6fc18f27b7d9e0ad57fcdccfe9f21e5d8a6e8ce3
Sergey <xsergey87x@ukr.net>
2022-12-24 19:05:41 +0200

    c1_2

--- cursor/src/main/java/com/cursor/cursor/HwKafkaApplication.java
+++ cursor/src/main/java/com/cursor/cursor/HwKafkaApplication.java
@@ -1,8 +1,11 @@
 package com.cursor.cursor;
 
 import com.cursor.cursor.kafka.Producer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.Properties;
 
@@ -13,7 +16,13 @@ public class HwKafkaApplication {
 		SpringApplication.run(HwKafkaApplication.class, args);
 	}
 
-
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate)
+	{
+		return args -> {
+			kafkaTemplate.send("fresh-news", "test-news");
+		};
+	}
 
 
 }
