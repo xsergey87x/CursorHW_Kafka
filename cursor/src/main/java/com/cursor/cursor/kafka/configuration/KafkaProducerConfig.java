dcffd4038c9b62ba2128121c50f976397501aa8c
Sergey <xsergey87x@ukr.net>
2022-12-24 12:33:21 +0200

    c1_1

--- cursor/src/main/java/com/cursor/cursor/kafka/configuration/KafkaProducerConfig.java
+++ cursor/src/main/java/com/cursor/cursor/kafka/configuration/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.cursor.cursor.kafka.configuration;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -26,10 +27,13 @@ public Map<String, Object> producerConfiguration()
    return props;
 }
 
-public ProducerFactory<String,String> producerFactory()
-{
+@Bean
+public ProducerFactory<String,String> producerFactory(){
 return new DefaultKafkaProducerFactory<>(producerConfiguration());
 }
 
-public KafkaTemplate<String, String>
+@Bean
+public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String,String> producerFactory) {
+    return new KafkaTemplate<>(producerFactory);
+}
 }
